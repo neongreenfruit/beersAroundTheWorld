3814b740ac78bce5a6626482c8d4cd2af5032416
mrmusselman1 <info@matthewrmusselman.com>
2015-12-31 13:31:52 -0500

    Some pretty print stuff and working on routes

--- beers/beers.js
+++ beers/beers.js
@@ -1,5 +1,95 @@
+// Overhead stuff
+var eve = "December 31, 2015 ";
+var beerlist = [
+  {
+    beer: "IPA",
+    brewery: "Omission",
+    abv: 6.7,
+    ibu: 65,
+    hop: false,
+    description: "Omission IPA is a bright, hop forward Northwest Style IPA produced in the spirit of the original IPAs shipped from the UK to India in the late 1800’s. The heavy-handed use of Cascade and Summit hops give it notable pine, citrus, and grapefruit aromas and flavors. The bitterness is what you would expect of a NW IPA but this beer is balanced and smooth due to the perfect level of malt sweetness. The finish is crisp, clean, and refreshing – it’s a true IPA lover’s IPA.",
+    tasting: "TBD",
+    pairing: "TBD",
+    time: new Date(eve + "18:20"),
+  },
+  {
+    beer: "Lager",
+    brewery: "Omission",
+    abv: 4.6,
+    ibu: 20,
+    hop: false,
+    description: "Omission Lager is a refreshing and crisp beer, brewed in the traditional lager style. Perfect for a variety of beer drinking occasions, Omission Lager’s aromatic hop profile offers a unique, easy-drinking beer for those looking for a lighter and approachable beer style.",
+    tasting: "TBD",
+    pairing: "TBD",
+    time: new Date(eve + "18:00"),
+  },
+  {
+    beer: "Something Else",
+    brewery: "Crazy",
+    abv: 6.7,
+    ibu: 65,
+    hop: false,
+    description: "Omission IPA is a bright, hop forward Northwest Style IPA produced in the spirit of the original IPAs shipped from the UK to India in the late 1800’s. The heavy-handed use of Cascade and Summit hops give it notable pine, citrus, and grapefruit aromas and flavors. The bitterness is what you would expect of a NW IPA but this beer is balanced and smooth due to the perfect level of malt sweetness. The finish is crisp, clean, and refreshing – it’s a true IPA lover’s IPA.",
+    tasting: "TBD",
+    pairing: "TBD",
+    time: new Date(eve + "18:40"),
+  },
+  {
+    beer: "Super Duper",
+    brewery: "Candy",
+    abv: 4.6,
+    ibu: 20,
+    hop: false,
+    description: "Omission Lager is a refreshing and crisp beer, brewed in the traditional lager style. Perfect for a variety of beer drinking occasions, Omission Lager’s aromatic hop profile offers a unique, easy-drinking beer for those looking for a lighter and approachable beer style.",
+    tasting: "TBD",
+    pairing: "TBD",
+    time: new Date(eve + "19:00"),
+  },
+  {
+    beer: "Markers",
+    brewery: "Candy",
+    abv: 4.6,
+    ibu: 20,
+    hop: false,
+    description: "Omission Lager is a refreshing and crisp beer, brewed in the traditional lager style. Perfect for a variety of beer drinking occasions, Omission Lager’s aromatic hop profile offers a unique, easy-drinking beer for those looking for a lighter and approachable beer style.",
+    tasting: "TBD",
+    pairing: "TBD",
+    time: new Date(eve + "19:20"),
+  },
+  {
+    beer: "Markers",
+    brewery: "Oooops",
+    abv: 4.6,
+    ibu: 20,
+    hop: false,
+    description: "Omission Lager is a refreshing and crisp beer, brewed in the traditional lager style. Perfect for a variety of beer drinking occasions, Omission Lager’s aromatic hop profile offers a unique, easy-drinking beer for those looking for a lighter and approachable beer style.",
+    tasting: "TBD",
+    pairing: "TBD",
+    time: new Date(eve + "19:40"),
+  },
+  {
+    beer: "Markers",
+    brewery: "Oooops",
+    abv: 4.6,
+    ibu: 20,
+    hop: false,
+    description: "Omission Lager is a refreshing and crisp beer, brewed in the traditional lager style. Perfect for a variety of beer drinking occasions, Omission Lager’s aromatic hop profile offers a unique, easy-drinking beer for those looking for a lighter and approachable beer style.",
+    tasting: "TBD",
+    pairing: "TBD",
+    time: new Date(eve + "20:00"),
+  },
+];
+
+// Mongo DB Collections
 Beers = new Mongo.Collection("beers");
 
+// Routes
+Router.route('/:time', {
+  data : function() {
+    console.log("time page " + this.params.time);
+
+  }
+});
 
 if (Meteor.isClient) {
   // // counter starts at 0
@@ -18,9 +108,20 @@ if (Meteor.isClient) {
   //   }
   // });
 
-  // Access the beer list from all templates
+  // Access the beer list from all templates (sort early to late)
   Template.registerHelper("beers",function(){
-    return Beers.find({});
+    return Beers.find({}, {sort: {time: 1}});
+  });
+
+  // Converts a date time to hh:mm am/pm time
+  Template.registerHelper("friendlyTime", function(dateTime) {
+    return dateTime.toLocaleTimeString().replace(/:\d+ /, ' ');
+  });
+
+  // Determines if a time is the top of the hour
+  Template.registerHelper("isHour", function(dateTime) {
+    var minutes = dateTime.getMinutes();
+    return dateTime.getMinutes() == 0;
   });
 
   Template.body.helpers({
@@ -47,11 +148,6 @@ if (Meteor.isClient) {
         open : false,
       },
     ],
-
-    // beers : function() {
-    //   return Beers.find({});
-    // }
-
   });
 
   Template.ratingPanel.helpers({
@@ -78,46 +174,15 @@ if (Meteor.isClient) {
       }
     ],
   });
-
 }
 
 if (Meteor.isServer) {
+  // code to run on server at startup
   Meteor.startup(function () {
-    // code to run on server at startup
-
-    // Prep the beer list
-    var eve = '2015-12-31';
-    var beerlist = [
-        {
-          name: "Lager",
-          brewery: "Omission",
-          abv: 4.6,
-          ibu: 20,
-          hop: false,
-          description: "Omission Lager is a refreshing and crisp beer, brewed in the traditional lager style. Perfect for a variety of beer drinking occasions, Omission Lager’s aromatic hop profile offers a unique, easy-drinking beer for those looking for a lighter and approachable beer style.",
-          tasting: "TBD",
-          pairing: "TBD",
-          time: Date(eve + "T18:00"),
-        },
-        {
-          name: "IPA",
-          brewery: "Omission",
-          abv: 6.7,
-          ibu: 65,
-          hop: false,
-          description: "Omission IPA is a bright, hop forward Northwest Style IPA produced in the spirit of the original IPAs shipped from the UK to India in the late 1800’s. The heavy-handed use of Cascade and Summit hops give it notable pine, citrus, and grapefruit aromas and flavors. The bitterness is what you would expect of a NW IPA but this beer is balanced and smooth due to the perfect level of malt sweetness. The finish is crisp, clean, and refreshing – it’s a true IPA lover’s IPA.",
-          tasting: "TBD",
-          pairing: "TBD",
-          time: Date(eve + "T18:20"),
-        },
-    ];
-
     // Insert all the beers into the collection
     Beers.remove({});
     beerlist.forEach(function(beer) {
-        Beers.insert(beer);
+      Beers.insert(beer);
     });
-
-
   });
 }
